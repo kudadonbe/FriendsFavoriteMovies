71bf845721220dc63562974461df5ee044cf938c
Kudadonbe <hussain.shareef@gmail.com>
2025-01-28 18:24:12 +0500

    Section 2 Use detail views as navigation destinations Now that you have detail views to edit your data, you can use them in the navigation hierarchy. In this section, you’ll update each list view to let people select a friend or movie and edit it in a detail view.

---
+++ FriendsFavoriteMovies/FriendDetail.swift
@@ -0,0 +1,18 @@
+//
+//  FriendDetail.swift
+//  FriendsFavoriteMovies
+//
+//  Created by Hussain Shareef on 1/28/25.
+//
+
+import SwiftUI
+
+struct FriendDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FriendDetail()
+}
